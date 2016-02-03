a5d1efdf8eb430a86219a43fae9d99cb49fb1586
Caleb Davenport <caleb@calebd.me>
2016-02-03 15:07:52 -0800

    Replace Slate with Hammerspoon.

---
+++ Dotfiles/hammerspoon/init.lua
@@ -0,0 +1,30 @@
+hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "Up", function()
+    local window = hs.window.focusedWindow()
+    local screen = window:screen():frame()
+    local frame = hs.geometry.rect(screen.x + 10, screen.y + 10, screen.w - 20, screen.h - 20)
+    window:setFrame(frame)
+end)
+
+hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "Left", function()
+    local window = hs.window.focusedWindow()
+    local screen = window:screen():frame()
+    local frame = hs.geometry.rect(screen.x + 10, screen.y + 10, (screen.w / 2) - 10, screen.h - 20)
+    window:setFrame(frame)
+end)
+
+hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "Right", function()
+    local window = hs.window.focusedWindow()
+    local screen = window:screen():frame()
+    local frame = hs.geometry.rect(screen.w / 2, screen.y + 10, (screen.w / 2) - 10, screen.h - 20)
+    window:setFrame(frame)
+end)
+
+hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "Down", function()
+    local window = hs.window.focusedWindow()
+
+    local frame = window:frame()
+    frame.w = 1000
+    frame.h = 800
+
+    window:setFrame(frame)
+end)
