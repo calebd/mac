8a207d4ac4fa19fedaa6c5f85b8ff52187b1dda1
Caleb Davenport <caleb@calebd.me>
2016-04-18 11:30:08 -0700

    Remove Xcode names from prompt for now.

--- Dotfiles/zsh/prompt.zsh
+++ Dotfiles/zsh/prompt.zsh
@@ -1,25 +1,28 @@
 cmd_prompt_red_color() {
 	echo "%{$fg[red]%}"
 }
+
 cmd_prompt_reset_color() {
 	echo "%{$reset_color%}"
 }
-cmd_prompt_xcode_name() {
-	workspace="$(ls | grep '.*\.xcworkspace$' | cut -d '.' -f1)"
-	if [ -n "$workspace" ]; then
-		echo "$workspace"
-		return;
-	fi
-	
-	project="$(ls | grep '.*\.xcodeproj$' | cut -d '.' -f1)"
-	if [ -n "$project" ]; then
-		echo "$project"
-		return;
-	fi
-	
+
+cmd_prompt_project_name() {
+	# workspace="$(ls | grep '.*\.xcworkspace$' | cut -d '.' -f1)"
+	# if [ -n "$workspace" ]; then
+	# 	echo "$workspace"
+	# 	return;
+	# fi
+
+	# project="$(ls | grep '.*\.xcodeproj$' | cut -d '.' -f1)"
+	# if [ -n "$project" ]; then
+	# 	echo "$project"
+	# 	return;
+	# fi
+
 	directory="$(basename $PWD)"
 	echo "$directory"
 }
+
 cmd_prompt_git_branch() {
   if [ ! git ls-files >& /dev/null ]; then
 		return;
@@ -28,4 +31,4 @@ cmd_prompt_git_branch() {
   echo " (${ref#refs/heads/})"
 }
 
-PROMPT='$(cmd_prompt_red_color)$(cmd_prompt_xcode_name)$(cmd_prompt_git_branch) -> $(cmd_prompt_reset_color)'
+PROMPT='$(cmd_prompt_red_color)$(cmd_prompt_project_name)$(cmd_prompt_git_branch) -> $(cmd_prompt_reset_color)'
