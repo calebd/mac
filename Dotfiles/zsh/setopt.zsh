a1516872b5d895026e7cd51d982b0cd65cd08fd1
Caleb Davenport <caleb@calebd.me>
2016-09-21 11:54:25 -0400

    Update history options.

--- Dotfiles/zsh/setopt.zsh
+++ Dotfiles/zsh/setopt.zsh
@@ -1,2 +1,11 @@
 # Prompt
-setopt PROMPT_SUBST
+setopt prompt_subst
+
+# History
+setopt hist_verify
+setopt share_history
+setopt extended_history
+setopt append_history
+setopt inc_append_history
+setopt hist_ignore_dups
+setopt hist_reduce_blanks
