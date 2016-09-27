0c41d1fe374c2b08f45bca143184e78ed209884d
Caleb Davenport <caleb@calebd.me>
2016-09-27 19:10:44 -0400

    Restore current working directory in terminal title bar.

--- Dotfiles/zsh/functions.zsh
+++ Dotfiles/zsh/functions.zsh
@@ -7,3 +7,22 @@ x() {
 c() {
   cd ~/Code/$1;
 }
+
+# Set Apple Terminal.app resume directory
+# if [[ $TERM_PROGRAM == "Apple_Terminal" ]] && [[ -z "$INSIDE_EMACS" ]] {
+#   function chpwd {
+# 	local SEARCH=' '
+# 	local REPLACE='%20'
+# 	local PWD_URL="file://$HOSTNAME${PWD//$SEARCH/$REPLACE}"
+# 	printf '\e]7;%s\a' "$PWD_URL"
+#   }
+#   chpwd
+# }
+_set_terminal_location() {
+  local SEARCH=' '
+  local REPLACE='%20'
+  local PWD_URL="file://$HOSTNAME${PWD//$SEARCH/$REPLACE}"
+  printf '\e]7;%s\a' "$PWD_URL"
+}
+chpwd_functions+=(_set_terminal_location)
+_set_terminal_location
